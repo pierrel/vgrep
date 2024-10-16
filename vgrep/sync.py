898c3f8093f2a775b30de5277aff0ef7111ed380
Pierre Larochelle <pierre@quirozlarochelle.com>
2024-10-16 06:39:36 -0700

    Initial commit

---
+++ vgrep/sync.py
@@ -0,0 +1,21 @@
+from db import DB
+from fs import FS
+from chromadb import chromadb
+from file_sync import FileSync
+from pathlib import Path
+import pdb
+
+# point to FS
+fs = FS([Path('/home/pierre/Documents')])
+
+# set up DB
+client = chromadb.PersistentClient(path="./db")
+collection = None
+try: collection =  client.get_collection(name="main")
+except chromadb.errors.InvalidCollectionException:
+    collection = client.create_collection(name="main")
+db = DB(collection)
+
+# create the sync
+fsync = FileSync(fs, db)
+fsync.sync()
