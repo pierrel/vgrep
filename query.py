10dd6953757ffa77068e8b6938f6bdd50812ca39
Pierre Larochelle <pierre@quirozlarochelle.com>
2025-03-03 06:45:10 -0800

    Moving script stuff into __main__

--- query.py
+++ query.py
@@ -1,29 +1,31 @@
-import argparse
 from vgrep.db import DB
 from settings import parse_settings
 from vgrep.file_sync import FileSync
 from chromadb import chromadb
 from pathlib import Path
+from typing import List
 
 settings = parse_settings('./settings.json')
 
-# get the search string
-parser = argparse.ArgumentParser()
-parser.add_argument("search",
-                    help="The search string to use for the query")
-args = parser.parse_args()
-
 # set up DB
-settings = chromadb.Settings(anonymized_telemetry=False)
+chroma_settings = chromadb.Settings(anonymized_telemetry=False)
 client = chromadb.PersistentClient(path=settings['db_dir'],
-                                   settings=settings)
+                                   settings=chroma_settings)
 collection = None
 try:
-    collection =  client.get_collection(name="main")
+    collection = client.get_collection(name="main")
 except chromadb.errors.InvalidCollectionException:
     collection = client.create_collection(name="main")
 db = DB(collection)
 
-for res in db.query(args.search):
-    print(f'{res["filename"]}:{res["line_start"]}')
-    print(f'{res["text"]}\n\n')
+if __name__ == "__main__":
+    import argparse
+    # get the search string
+    parser = argparse.ArgumentParser()
+    parser.add_argument("search",
+                        help="The search string to use for the query")
+    args = parser.parse_args()
+    
+    for res in db.query(args.search):
+        print(f'{res["filename"]}:{res["line_start"]}')
+        print(f'{res["text"]}\n\n')
