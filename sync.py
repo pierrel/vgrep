ea876536a2a633a802406d46c8e8d9f1453e5f7e
Pierre Larochelle <pierre@quirozlarochelle.com>
2025-03-03 06:45:34 -0800

    More moving script code into __main__

--- sync.py
+++ sync.py
@@ -4,23 +4,26 @@ from settings import parse_settings
 from chromadb import chromadb, Settings
 from vgrep.file_sync import FileSync
 from pathlib import Path
-import pdb
 
 settings = parse_settings('./settings.json')
 
 # point to FS
-fs = FS(settings['sync_dirs'].keys())
+paths = map(Path,
+            settings['sync_dirs'].keys())
+fs = FS(paths)
 
 # set up DB
 chroma_settings = chromadb.Settings(anonymized_telemetry=False)
 client = chromadb.PersistentClient(path=settings['db_dir'],
                                    settings=chroma_settings)
 collection = None
-try: collection =  client.get_collection(name="main")
+try: collection = client.get_collection(name="main")
 except chromadb.errors.InvalidCollectionException:
     collection = client.create_collection(name="main")
 db = DB(collection)
 
 # create the sync
 fsync = FileSync(fs, db)
-fsync.sync()
+
+if __name__ == "__main__":
+    fsync.sync()
