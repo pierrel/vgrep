7decb7ebc2b51542ea6b50bafac812e58fd24872
Pierre Larochelle <pierre@quirozlarochelle.com>
2025-05-26 15:42:29 -0700

    Simplifying contextualizer

--- vgrep/contextualizer.py
+++ vgrep/contextualizer.py
@@ -1,64 +1,17 @@
 from vgrep.templater import Templater
-from typing import Literal
 from langchain_core.runnables import Runnable
-from langgraph.graph import END, StateGraph, START
-from pydantic import BaseModel
 from typing import Generator
-from functools import partial
 
 
 def contextualize(input_gen: Generator[str],
                   contextualizing_llm: Runnable) -> str:
-    graph = contextualize_graph(contextualizing_llm)
-    res = graph.invoke({"context": "",
-                        "to_contextualize": input_gen,
-                        "done_contextualizing": False},
-                       {"recursion_limit": 100})
-    return res['context']
+    templater = Templater("contextualize.txt.j2")
+    context = ""
 
-
-class ContextState(BaseModel):
-    context: str
-    to_contextualize: Generator[str]
-    done_contextualizing: bool
-
-
-def contextualize_chunk(llm: Runnable,
-                        state: ContextState) -> ContextState:
-    """Contextualizing the next chunk of the `state`'s `to_contextualize`
-    string and puts it into `state.context`
-
-    """
-    try:
-        chunk = next(state.to_contextualize)
-        templater = Templater()
-        request = templater.render_template("contextualize.txt.j2",
-                                            state=state,
+    for chunk in input_gen:
+        request = templater.render_template(context=context,
                                             chunk=chunk)
-        res = llm.invoke(request)
-        state.context = res.content
-    except StopIteration:
-        state.done_contextualizing = True
-    return state
-
-
-def done_contextualizing(state: ContextState) -> Literal[END, "contextualize"]:
-    """Decides whether contextualization should continue or if the
-    graph should terminate
-
-    """
-    if state.done_contextualizing:
-        return END
-    else:
-        return "contextualize"
-
-
-def contextualize_graph(contextualizer: Runnable) -> StateGraph:
-    """Returns a graph that can be invoked to contextualize a string"""
-    contextualize_node = partial(contextualize_chunk, contextualizer)
-    workflow = StateGraph(ContextState)
-    workflow.add_node("contextualize", contextualize_node)
-    workflow.add_edge(START, "contextualize")
-    workflow.add_conditional_edges("contextualize",
-                                   done_contextualizing)
-    return workflow.compile()
+        res = contextualizing_llm.invoke(request)
+        context = res.content
+        
+    return context
