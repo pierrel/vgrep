d0b22bcf52e4e8cd3163f014704c00fa0a4469c1
Pierre Larochelle <pierre@quirozlarochelle.com>
2025-05-26 15:42:49 -0700

    Making templater easier to use

--- vgrep/templater.py
+++ vgrep/templater.py
@@ -2,15 +2,12 @@ from jinja2 import Environment, PackageLoader, select_autoescape
 
 
 class Templater:
-    def __init__(self):
+    def __init__(self, template_name):
         self.env = Environment(
             loader=PackageLoader("vgrep"),
             autoescape=select_autoescape()
         )
+        self.template = self.env.get_template(template_name)
 
-    def render_template(self,
-                        template_name: str,
-                        **params):
-        template = self.env.get_template(template_name)
-
-        return template.render(**params)
+    def render_template(self, **params):
+        return self.template.render(**params)
